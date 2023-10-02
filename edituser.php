74ec908fc2a0943c78738136e4e449442f7515be
RIjvan-Juneja <junejarijvan8128@gmail.com>
2023-10-02 11:05:20 +0530

    Update edituser.php

--- edituser.php
+++ edituser.php
@@ -22,6 +22,24 @@
         <option value="Male" <?php if($rows["gender"] == "Male") echo "selected"; ?> >Male</option>
         <option value="Female" <?php if($rows["gender"] == "Female") echo "selected"; ?> >Female</option>
       </select> <br>
+      <label for="hobbies">Hobbies:</label><br>
+      <input type="checkbox" id="hobby1" name="hobbies[]" value="Reading">
+      <label for="hobby1">Reading</label>
+      <input type="checkbox" id="hobby2" name="hobbies[]" value="Gaming">
+      <label for="hobby2">Gaming</label>
+      <input type="checkbox" id="hobby3" name="hobbies[]" value="Cooking">
+      <label for="hobby3">Cooking</label><br>
+
+      <label for="interests">Interests:</label><br>
+      <select id="interests" name="interests[]" multiple>
+        <option value="Music" >Music</option>
+        <option value="Sports">Sports</option>
+        <option value="Travel">Travel</option>
+        <option value="Movies">Movies</option>
+      </select><br>
+
+      <label for="about">About Your self : </label><br>
+      <textarea name="about" id="about" cols="30" rows="10"></textarea><br>
       <button type="button" onclick="submitData('edit');">Edit</button>
     </form>
     <br>
